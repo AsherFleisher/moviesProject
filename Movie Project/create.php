84d1838ef51bab95bc8290a7ac8edda3da547d78
AsherFleisher <asherfleisherr@gmail.com>
2017-09-10 09:12:00 +0300

    Update and rename create.html to create.php

--- Movie Project/create.php
+++ Movie Project/create.php
@@ -6,6 +6,10 @@
 </head>
 
 <body>
+    
+    <?php 
+    require_once "header.php";
+    ?>
 
     Name of new movie<input id="movieName">
     <button name="createMovie" onclick="ajax(this)">Add new movie</button>
@@ -15,10 +19,11 @@
     <a href='index.html'> <button>Back to home page</button></a>
 
     <script src="ajax.js"></script>
-
-
+    
+     <?php 
+    require_once "footer.php";
+    ?>
 
 </body>
-
 
 </html>
